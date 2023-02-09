66f4c3cca4338d6c29f4f7a5c392057ae4a3d7dd
hyunhoh <skins0329@gmail.com>
2023-02-09 20:39:08 +0900

    basename 수정

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Footer from '../src/components/footer.js';
-import Home from "./page/home.js";
-import { Route, Routes } from "react-router-dom";
-import Login from "./page/login.js";
-import MyPage from "./page/myPage.js";
+import Home from './page/home.js';
+import { Route, Routes } from 'react-router-dom';
+import Login from './page/login.js';
+import MyPage from './page/myPage.js';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/mypage' element={<MyPage />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
+        console.log('되라')
       </Routes>
       <Footer />
     </>
-  
-  )
+  );
 }
 
 export default App;
